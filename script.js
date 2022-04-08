815e89aeaec9c68333982e3c48ec4a1c6304170f
brualvess <oliveirabrunna2210@gmail.com>
2022-04-08 11:56:37 -0300

    distribuição de cartas

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
-function ditribuicaoDeCartas() {
+function distribuicaoDeCartas() {
   let cartas = prompt("Com quantas cartas você quer jogar?");
-  while(cartas % 2 != 0 || cartas > 14 || cartas < 4 ){
-    cartas = prompt("Com quantas cartas você quer jogar?")
+  while (cartas % 2 != 0 || cartas > 14 || cartas < 4) {
+    cartas = prompt("Com quantas cartas você quer jogar?");
+  }
+  distribuirCartas(cartas);
+}
+
+function selecionaCartas(cartas, numeroDeCartas) {
+  const selecionadas = [];
+  const unicas = numeroDeCartas / 2;
+  const comparar = comparador();
+  for (let indice = 0; indice < unicas; indice++) {
+    selecionadas.push(cartas[indice]);
+    selecionadas.push(cartas[indice]);
+  }
+  return selecionadas.sort(comparador);
+}
+
+function distribuirCartas(numeroDeCartas) {
+  const cartas = [
+    "explodyparrot",
+    "bobrossparrot",
+    "fiestaparrot",
+    "metalparrot",
+    "revertitparrot",
+    "tripletsparrot",
+    "unicornparrot",
+  ];
+  let selecionadas = selecionaCartas(cartas, numeroDeCartas)
+  const conteudo = document.querySelector(".conteudoLayout");
+  for (let indice = 0; indice < selecionadas.length; indice++) {
+    conteudo.innerHTML += `
+    <div class="cartasLayout">
+    <div class="cartas">
+      <img class="gif" src="imagens/${selecionadas[indice]}.gif" alt="" />
+      <img class="papagaio" src="imagens/front.png" alt="" />
+    </div>
+  </div>
+    `;
   }
 }
-ditribuicaoDeCartas();
+function comparador() {
+  return Math.random() - 0.5;
+}
