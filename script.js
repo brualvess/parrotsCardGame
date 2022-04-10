bf7b2d5a525a7d8bd9de7a735664fcd03c642655
brualvess <oliveirabrunna2210@gmail.com>
2022-04-10 19:43:44 -0300

    jogo funcionando

--- script.js
+++ script.js
@@ -27,13 +27,13 @@ function distribuirCartas(numeroDeCartas) {
     "tripletsparrot",
     "unicornparrot",
   ];
-  let selecionadas = selecionaCartas(cartas, numeroDeCartas)
+  let selecionadas = selecionaCartas(cartas, numeroDeCartas);
   const conteudo = document.querySelector(".conteudoLayout");
   for (let indice = 0; indice < selecionadas.length; indice++) {
     conteudo.innerHTML += `
     <div class="cartasLayout" onclick="virarCarta(this)">
     <div class="cartas">
-      <img class="gif" src="imagens/${selecionadas[indice]}.gif" alt="" />
+      <img class="gif ${selecionadas[indice]}" src="imagens/${selecionadas[indice]}.gif" alt="" />
       <img class="papagaio"  src="imagens/front.png" alt="" />
     </div>
   </div>
@@ -44,7 +44,33 @@ function comparador() {
   return Math.random() - 0.5;
 }
 
-function virarCarta(elemento){
-  elemento.classList.add("cliquei")
-  
+function virarCarta(elemento) {
+  elemento.classList.add("cliquei");
+  virandoCartas();
 }
+
+function virandoCartas() {
+  const clicados = document.querySelectorAll(".cliquei");
+  if (clicados.length == 2) {
+    console.log("hiii")
+    let carta1 = clicados[0].querySelector(".gif").classList[1];
+    let carta2 = clicados[1].querySelector(".gif").classList[1];
+    console.log(carta1, carta2)
+    if (carta1 == carta2) {
+      clicados[0].classList.add("acertou");
+      clicados[1].classList.add("acertou");
+      clicados[0].classList.remove("cliquei")
+      clicados[1].classList.remove("cliquei")
+
+    } else {
+      console.log("hello")
+      setTimeout(desvirarCartas, 1000, clicados);
+    }
+  }
+}
+
+function desvirarCartas(clicados){
+  clicados[0].classList.remove("cliquei")
+  clicados[1].classList.remove("cliquei")
+}
+
